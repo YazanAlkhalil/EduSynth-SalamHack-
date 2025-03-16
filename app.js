99a8ace8eb211e624ba7cbe9000a05513bc7aea1
Ahmad <ahmadzer607@gmail.com>
2025-03-16 16:52:52 +0300

    nothing

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something went wrong!' });
 });
 console.log("hi2");
-
+              
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
