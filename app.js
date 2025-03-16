dab9d5e36ff3ea193edb7fccb2fcf16ef9558f07
Ahmad <ahmadzer607@gmail.com>
2025-03-16 23:27:24 +0300

    test

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
-console.log("hi3");
+console.log("hi4");
               
 // Start server
 const PORT = process.env.PORT || 3000;
