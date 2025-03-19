9b3bd8a870d3301eb31ad6bd8f83b57cdbc2e1cf
yazan.alkhalil <yazan.alkhalil@90-soft.com>
2025-03-19 20:08:14 +0300

    as

--- api/app.js
+++ api/app.js
@@ -20,7 +20,6 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
-console.log("hi4");
               
 // Start server
 const PORT = process.env.PORT || 3000;
