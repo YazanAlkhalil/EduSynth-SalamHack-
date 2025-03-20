b67ab9e25694246dfe60dc43904076e9416ea3b5
yazan.alkhalil <yazan.alkhalil@90-soft.com>
2025-03-20 13:46:31 +0300

    as

--- api/app.js
+++ api/app.js
@@ -30,7 +30,7 @@ app.use((err, req, res, next) => {
 const PORT =  3005;
 const http = require('http');
 const server = http.createServer(app);
-server.timeout = 600000000; // 6000000ms = 6000 seconds
+server.timeout = 0; // 6000000ms = 6000 seconds
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
